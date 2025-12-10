14a96d4472960779fa144d9fddb4ef62ce9236da
Prathamesh Kshirsagar <erprathameshkshirsagar@gmail.com>
2025-12-10 20:34:22 +0530

    Resolve merge conflicts and update settings.py

--- Cameraoperators/settings.py
+++ Cameraoperators/settings.py
@@ -131,7 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/var/www/cameraoperators/static/'
 
 
 # Default primary key field type
